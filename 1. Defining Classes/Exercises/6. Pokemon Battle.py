052d8b139f8eee443b4c9b11650a62c9a9e710f6
Borislav-source <tsv.borislav@gmail.com>
2021-02-23 13:46:33 +0200

    My solutions

---
+++ 1. Defining Classes/Exercises/6. Pokemon Battle.py
@@ -0,0 +1,46 @@
+class Pokemon:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def pokemon_details(self):
+        return f"{self.name} with health {self.health}"
+
+
+class Trainer:
+    pokemon = []
+    names = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_pokemon(self, pokemon):
+        if pokemon in self.pokemon:
+            return f"This pokemon is already caught"
+        self.pokemon.append(pokemon)
+        details = Pokemon.pokemon_details(pokemon)
+        return f"Caught {details}"
+
+    def release_pokemon(self, name):
+        if name == pokemon.name:
+            self.pokemon.remove(pokemon)
+            return f"You have released {name}"
+        return "Pokemon is not caught"
+
+    def trainer_data(self):
+        data = f"""Pokemon Trainer {self.name}
+Pokemon count {len(self.pokemon)}
+"""
+        for poke in self.pokemon:
+            data += f"- {Pokemon.pokemon_details(poke)}\n"
+        return data
+
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
---
+++ 1. Defining Classes/Exercises/7. Guild_System.py
@@ -0,0 +1,62 @@
+class Player:
+    skills = {}
+    guild = "Unaffiliated"
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return f"Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        data = f"""Name: {self.name}
+Guild: {self.guild}
+HP: {self.hp}
+MP: {self.mp}
+"""
+        for skill in self.skills:
+            data += f"==={skill} - {self.skills[skill]}\n"
+
+        return data
+
+
+class Guild:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player):
+        if player in self.players:
+            return f"Player {player} is already in the guild."
+        elif Player.guild == 'Unaffiliated':
+            self.players.append(player)
+            Player.guild = self.name
+            return f"Welcome player {player.name} to the guild {self.name}"
+        else:
+            return f"Player {player} is in another guild."
+
+    def kick_player(self, player):
+        if player not in self.players:
+            return f"Player {player} is not in the guild."
+        self.players.remove(player)
+        Player.guild = 'Unaffiliated'
+        return f"Player {player} has been removed from the guild."
+
+    def guild_info(self):
+        data = f"""Guild: {self.name}
+"""
+        for pl in self.players:
+            data += f"{Player.player_info(pl)}\n"
+        return data
+
+
+player = Player("George", 50, 100)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
