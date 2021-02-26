4450fdc1c476e0cb2c1787e78a8634df760551bc
Borislav-source <tsv.borislav@gmail.com>
2021-02-26 12:09:15 +0200

    My solutions

--- 2. Classes and Instances/Exercises/test.py
+++ 2. Classes and Instances/Exercises/test.py
@@ -1,35 +1,44 @@
-class Exercise:
-    text = 'Bobby'
+import math
+# class Exercise:
+#     text = 'Bobby'
+#
+#
+# print(Exercise.text)
+# x = Exercise()
+# print(x.text)
+# x.text = 'Vicky'
+# y = Exercise()
+# print(y.text)
+# print(x.text)
+# print(Exercise.text)
+# y.text = 'Boyko'
+# print(y.text)
+# Exercise.text = 'Madlena'
+# print(Exercise.text)
+# t = Exercise()
+# print(t.text)
+# Exercise.name = "Dobri"
+# print(Exercise.name)
+# print(Exercise)
+# y.name = 'Gogo'
+# print(Exercise.name)
+# print(y.name)
+#
+#
+# class MyClass:
+#     number = 743
+#
+#     def say_hello(self):
+#         return 'Hello'
+#
+#
+# x = MyClass()
+# print(x.say_hello())
 
+# print((10**2 + 2*10*3 - 3**2))
 
-print(Exercise.text)
-x = Exercise()
-print(x.text)
-x.text = 'Vicky'
-y = Exercise()
-print(y.text)
-print(x.text)
-print(Exercise.text)
-y.text = 'Boyko'
-print(y.text)
-Exercise.text = 'Madlena'
-print(Exercise.text)
-t = Exercise()
-print(t.text)
-Exercise.name = "Dobri"
-print(Exercise.name)
-print(Exercise)
-y.name = 'Gogo'
-print(Exercise.name)
-print(y.name)
-
-
-class MyClass:
-    number = 743
-
-    def say_hello(self):
-        return 'Hello'
-
-
-x = MyClass()
-print(x.say_hello())
+# print((10 - 3)**2 + (2 - 5)**2)
+#
+# print(math.sqrt(58))
+a = 23
+print(str(a).zfill(2))
