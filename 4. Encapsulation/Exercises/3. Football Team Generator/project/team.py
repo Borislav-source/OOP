72937d98bf0a8290ea9c167009c2cc4920be2f3e
Borislav-source <tsv.borislav@gmail.com>
2021-03-05 19:26:41 +0200

    Updates

--- 4. Encapsulation/Exercises/3. Football Team Generator/project/team.py
+++ 4. Encapsulation/Exercises/3. Football Team Generator/project/team.py
@@ -17,9 +17,37 @@ class Team:
         self.__name = new_name
 
     @property
-    def name(self):
-        return self.__name
+    def rating(self):
+        return self.__rating
 
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name
+    @rating.setter
+    def rating(self, new_rating):
+        self.__rating = new_rating
+
+    # @property
+    # def players(self):
+    #     return self.__players
+    #
+    # @players.setter
+    # def players(self, new_player):
+    #     self.__players.append(new_player)
+
+    def add_player(self, player):
+        if player in self.players:
+            return f"Player {player.name} has already joined"
+        self.players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def find_player(self, name):
+        for player in self.players:
+            if name == player.name:
+                return player
+
+    def remove_player(self, player_name: str):
+        player = self.find_player(player_name)
+        if not player:
+            return f"Player {player_name} not found"
+        self.players.remove(player)
+        return player
+
+
