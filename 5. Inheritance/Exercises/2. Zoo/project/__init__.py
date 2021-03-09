717c51c41e628a624fae68b798e16788d901582a
Borislav-source <tsv.borislav@gmail.com>
2021-03-09 11:17:47 +0200

    My solutions

---
+++ 5. Inheritance/Exercises/2. Zoo/project/__init__.py
@@ -0,0 +1,17 @@
+# from project.animal import Animal
+# from project.mammal import Mammal
+# from project.reptile import Reptile
+# from project.lizard import Lizard
+# from project.snake import Snake
+# from project.gorilla import Gorilla
+# from project.bear import Bear
+#
+#
+# mammal = Mammal("Stella")
+# print(mammal.__class__.__bases__[0].__name__)
+# print(mammal.name)
+# print(mammal._Animal__name)
+# lizard = Lizard("John")
+# print(lizard.__class__.__bases__[0].__name__)
+# print(lizard.name)
+# print(lizard._Animal__name)
