50916fc2cdf235990b769a726cec87a389b00560
Borislav-source <tsv.borislav@gmail.com>
2021-03-11 21:32:43 +0200

    My solutions

---
+++ 6. Polymorphism/Lab/3. Shapes.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius**2
+
+    def calculate_perimeter(self):
+        return 2*(pi * self.__radius)
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__width * self.__height
+
+    def calculate_perimeter(self):
+        return 2*self.__width + 2*self.__height
+
+
+circle = Circle(1)
+print(circle.calculate_area())
+print(circle.calculate_perimeter())
+rectangle = Rectangle(10, 20)
+print(rectangle.calculate_area())
+print(rectangle.calculate_perimeter())
+
+
+a = 7
+b = 6
+print(a.__trunc__())
