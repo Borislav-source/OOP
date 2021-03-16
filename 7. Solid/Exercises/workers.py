84603472d5893d685ef811dced2a92ac2c26bf3d
Borislav-source <tsv.borislav@gmail.com>
2021-03-16 12:19:02 +0200

    My solutions

---
+++ 7. Solid/Exercises/workers.py
@@ -0,0 +1,54 @@
+from abc import ABC
+
+
+class Worker(ABC):
+
+    @staticmethod
+    def work():
+        print("I'm working!!")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+
+class SuperWorker(Worker):
+
+    @staticmethod
+    def work():
+        print("I work very hard!!!")
+
+
+# worker = Worker()
+# manager = Manager()
+# manager.set_worker(worker)
+# manager.manage()
+#
+# super_worker = SuperWorker()
+# try:
+#     manager.set_worker(super_worker)
+# except AssertionError:
+#     print("manager fails to support super_worker....")
+
+# worker = Worker()
+# manager = Manager()
+# manager.set_worker(worker)
+# manager.manage()
+#
+# super_worker = SuperWorker()
+# try:
+#     manager.set_worker(super_worker)
+#     manager.manage()
+# except AssertionError:
+#     print("manager fails to support super_worker....")
